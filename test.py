b2d70f58c29ee4e61264ec903acfca7392e4840e
Fritz-D <104021119+Fritz-D@users.noreply.github.com>
2024-06-12 12:03:36 +0200

    print prettily

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from codegleu import calc_codegleu
 
 
 SOURCE = "/codegleu/data/testsource.txt"
-HYPOTHESIS = "/codegleu/data/testhypo1.txt"
+HYPOTHESES = "/codegleu/data/testhypo1.txt"
 REFERENCES = ["/codegleu/data/testref1.txt", "/codegleu/data/testref2.txt"]
 NUMGRAM = 4
 
@@ -11,8 +11,25 @@ dir = os.getcwd()
 sources = [source for source in open(dir + SOURCE).readlines()]
 references = [[ref for ref in open(dir + ref).readlines()] for ref in REFERENCES]
 references = list(map(list, zip(*references))) # from column-wise to row-wise
-hypothesis = [hypo for hypo in open(dir + HYPOTHESIS).readlines()]
+hypotheses = [hypo for hypo in open(dir + HYPOTHESES).readlines()]
 n_weights = [1/NUMGRAM] * NUMGRAM
 
-gl = calc_codegleu(sources, references, hypothesis, "java", weights = n_weights)
-print(f"GLEU+: {gl}")
+runs: list[dict] = []
+runs.append({"title": "total"} | calc_codegleu(sources, references, hypotheses, "java", weights = n_weights))
+for index, (source, reference, hypothesis) in enumerate(zip(sources, references, hypotheses)):
+    runs.append({"title": f"row {index+1}"} | calc_codegleu([source], [reference], [hypothesis], "java", weights = n_weights))
+
+print(f"GLEU+")
+cols = list(runs[0].keys())
+if "title" in cols:
+    cols.remove("title")
+    cols.insert(0, "title")
+maxlen = max([len(c) for c in cols])
+for col in cols:
+    print(col + " " * (maxlen-len(col)), end = "")
+print("")
+for run in runs:
+    for col in cols:
+        val = str(run.get(col, None))
+        print(val + " " * (maxlen-len(val)), end = "")
+    print("")
